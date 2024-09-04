96f3625f3d2c7bbfbaf5c20cbbb0dee1fdc050c4
Rittipong <rittipong.ta.66@ubu.ac.th>
2024-09-04 16:06:59 +0700

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+	darkMode:"media",
 	content: [
 		'./templates/**/*.html'
 	],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily:{
+
+			notosansthai:["Noto Sans Thai"]
+			}
+		},
 	},
 	plugins: [],
 }
